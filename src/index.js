06164cfbda140bdb8937ad785af64e3001fc0c6b
Omer Faruk Aydın <65319640+lawyer-omer@users.noreply.github.com>
2021-09-10 22:10:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,59 +1,27 @@
-const tester = 'testing';
 const axios = require('axios').default;
-
-/* function
-function axiosTest() {
-  // create a promise for the axios request
-  const promise = axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-  .then(function (response) {
-    console.log(response.data.bitcoin.usd);
-  });
-}
-
-
-axiosTest()
-*/
-/*
-async function getUserData() {
-  try {
-      const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
-      console.log(response.data.bitcoin.usd);
-      return response.data.bitcoin.usd
-      
-  }
-  catch (error) {
-      console.log(error);
-  }
-}
-
-var asd = getUserData()
-*/
+const { WebClient } = require('@slack/web-api');
+const { RTMClient } = require('@slack/rtm-api');
+var express = require('express')
+var app = express()
 
 //module
-const options = {
-  method: 'GET',
-  url: `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd`,
-  responseType: 'arraybuffer'
-  };
 
 module.exports = async function App(context) {
   if (context.event.text == 'btc-usd') {
-    //await context.postMessage('asd');
-    await axios.request(options).then((response) => {
-      //console.log(response.data.bitcoin.usd)
-      return context.response;})
-    //const take = await App(context)
-    await context.postMessage('I will say Bitcoin price soon!');
+    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+      console.log(response.data.bitcoin.usd);
+    await context.postMessage(`Hi this is btc price ${response.data.bitcoin.usd} USD.`);
+  }
+
+  if (context.event.text == 'eth-usd') {
+    const responseEth = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+      console.log(responseEth.data.ethereum.usd);
+    await context.postMessage(`Hi this is eth price ${responseEth.data.ethereum.usd} USD.`);
+  }
+
+  if (context.event.text == 'doge-usd') {
+    const responseDoge = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd');
+      console.log(responseDoge.data.dogecoin.usd);
+    await context.postMessage(`Hi this is doge price ${responseDoge.data.dogecoin.usd} USD.`);
   }
 }
-
-/*
-const options = {
-  method: 'GET',
-  url: `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd`}
-
-module.exports
-  if (context.event.text == 'btc-usd'){
-      return axios.request(options).then((response) => {
-          return response.data.bitcoin.usd;});
-          */
